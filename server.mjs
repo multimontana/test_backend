8b1a83d052f82fba0df01dfd7f67adb4a40c85c0
SurenMatinyan <suren.matinyan.94@mail.ru>
2021-05-19 00:31:44 +0400

    test

--- server.mjs
+++ server.mjs
@@ -2,17 +2,12 @@ import express from 'express';
 import cors from "cors"
 const app = express();
 const port = 3000;
-import test from './router/test.mjs';
+import testRouter from './router/test.mjs';
 app.listen(port,()=>{
     console.log(`🏃‍♀️ Server is running at http://localhost:${port}`)
-});
-
-
-app.get("/", (req, res) => {
-    res.send("hello");
 });
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use("/test", test)
+app.use("/test", testRouter)
